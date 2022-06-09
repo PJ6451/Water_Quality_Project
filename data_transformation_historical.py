21fe0b8cf4eb489f8d34741dc42fec160a58c88f
PJ6451 <arcpj6451@gmail.com>
2022-06-09 15:32:42 -0700

    got geomean working in under an hour for ~50Mb

--- data_transformation_historical.py
+++ data_transformation_historical.py
@@ -103,6 +103,7 @@ def data_transform(data: pandas.DataFrame, dic: dict) -> pandas.DataFrame:
     dates = data["SampleDate"]
     dates_split = dates.str.split(pat="T",expand=True)
     data["SampleDate"] = pandas.to_datetime(dates_split[0])
+    data["Result"] = data["Result"].abs()
 
     #### ADD SSW COLUMN ####
     data["Single Sample WQO"] = data["DW_AnalyteName"].map(dic)
@@ -120,14 +121,14 @@ def data_transform(data: pandas.DataFrame, dic: dict) -> pandas.DataFrame:
     data['Geomean45'] = 0
     data['Geomean60'] = 0
 
-    data = geomean.geo_mean(data)
+    data = geomean.gm_calc(data)
 
     return data
 
 #### LOAD DATA #####
-data_1969_2010    = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/18c57345-bf87-4c46-b358-b634d36be4d2/download/safetoswim_1969-2010_2022-06-01.csv')
-data_2010_2020    = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/7639446f-8c62-43d9-a526-8bc7952dd8bd/download/safetoswim_2010-2020_2022-06-01.csv')
-data_2020_present = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/1987c159-ce07-47c6-8d4f-4483db6e6460/download/safetoswim_2020-present_2022-06-01.csv')
+#data_1969_2010    = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/18c57345-bf87-4c46-b358-b634d36be4d2/download/safetoswim_1969-2010_2022-06-01.csv')
+#data_2010_2020    = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/7639446f-8c62-43d9-a526-8bc7952dd8bd/download/safetoswim_2010-2020_2022-06-01.csv')
+data_2020_present = read_data(r'https://data.ca.gov/dataset/6723ab78-4530-4e97-ba5e-6ffd17a4c139/resource/1987c159-ce07-47c6-8d4f-4483db6e6460/download/safetoswim_2020-present_2022-06-02.csv')
 
 #### DICTIONARY ####
 dic = {
@@ -135,17 +136,17 @@ dic = {
     "E. coli":320,
     "Coliform, Fecal":400,
     "Coliform, Total":10000,
-    "HF183":100
 }
 
 #### TRANSFORM DATA ####
-data1 = data_transform(data_1969_2010,dic)
-data2 = data_transform(data_2010_2020,dic)
+#data1 = data_transform(data_1969_2010,dic)
+#data2 = data_transform(data_2010_2020,dic)
 data3 = data_transform(data_2020_present,dic)
 
 #### COMINE DATA ####
-data = [data1,data2,data3]
-result = pandas.concat(data)
+#data = [data1,data2,data3]
+data = data3
+#result = pandas.concat(data)
 
 #### SAVE TO CSV ####
-result.to_csv('safetoswim_6-8-2022_transformed.csv',index=False)
+data.to_csv('safetoswim_6-8-2022_transformed.csv',index=False)
